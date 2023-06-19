1f8d619c30347804b99ec02e7198c3ef93f7123a
adan llanos <adanllanos96@gmail.com>
2023-06-19 14:14:57 -0400

    antes de probar la segunda vuelta

--- client/scripts/reporte.js
+++ client/scripts/reporte.js
@@ -1,7 +1,10 @@
 //Variable que guarda la cantidad de leyendas a insertar en el gráfico
 let cantidadLeyendas;
 var arregloDatos = [];
+var arregloDatosPorcentajes = [];
 var tieneDatosValidos = false;
+var primerCandidatoSegundaVuelta;
+var segundoCandidatoSegundaVuelta
 
 //Función que cargar el gràfico de Google
 function cargarGrafico() {
@@ -27,8 +30,16 @@ function drawChart() {
             return;
         }
     }
+    var totalVotos=0;
+    for (let i = 0; i < datos.length; i= i +2) {
+        const elemento = parseInt(datos[i + 1].textContent);
+        totalVotos=totalVotos+elemento;
+    }
+    console.log(totalVotos);
+
     var t = ['Gráfico', ''];
     arregloDatos.push(t);
+    //arregloDatosPorcentajes.push(t);
 
     for (i = 0; i < datos.length; i = i + 2) {
         //voy agregando los pares al arreglo arreglo arregloDatos.
@@ -36,6 +47,13 @@ function drawChart() {
         arregloDatos.push(t);
         tieneDatosValidos = true;
     }
+    for (i = 0; i < datos.length; i = i + 2) {
+        const elemento = parseInt(datos[i + 1].textContent);
+        const porcentaje = (elemento/totalVotos)*100;
+        t = [datos[i].textContent, porcentaje];
+        arregloDatosPorcentajes.push(t);
+    }
+    console.log(arregloDatosPorcentajes);
 
     //Genero la tabla que contiene los datos con el arreglo arregloDatos
     var data = google.visualization.arrayToDataTable(arregloDatos);
@@ -44,7 +62,7 @@ function drawChart() {
     var options = {
         'title': document.getElementById("titulo").value,
         'width': 700,
-        'height': 500
+        'height': 500,
     };
 
     // Muestro el gráfico dentro del elemento <div>  con id="piechart"
@@ -68,4 +86,24 @@ function drawChart() {
         chart.draw(data, options);
     }
 
+
+    /*
+    será proclamada a la Presidencia y Vicepresidencia la candidatura que haya reunido el 50% más uno de los votos válidos; o que haya obtenido un mínimo del 40% de los votos válidos con una diferencia de al menos 10% en relación con la segunda candidatura.
+    */ 
+    var resultadosOrdenados = arregloDatosPorcentajes.sort((a, b) => b[1] - a[1]);
+    var primerCandidatoRes = resultadosOrdenados[0][1];
+    var segundoCandidatoRes = resultadosOrdenados[1][1];
+    var diferenciaPorcentaje = primerCandidatoRes - segundoCandidatoRes;
+    var cumpleCondicionSegundaVuelta = (primerCandidatoRes >= 40 && diferenciaPorcentaje >= 10);
+    if (cumpleCondicionSegundaVuelta) {
+        primerCandidatoSegundaVuelta = resultadosOrdenados[0][0];
+        segundoCandidatoSegundaVuelta = resultadosOrdenados[1][0];
+        console.log("No habra segunda vuelta");
+        alert('El nuevo presidente es: '+ resultadosOrdenados[0][0])
+    } else {
+        primerCandidatoSegundaVuelta = resultadosOrdenados[0][0];
+        segundoCandidatoSegundaVuelta = resultadosOrdenados[1][0];
+        console.log("Habra segunda vuelta y los candidatos son: " + primerCandidatoSegundaVuelta + " y "+ segundoCandidatoSegundaVuelta);
+
+    }
 }
